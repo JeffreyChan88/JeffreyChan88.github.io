75e47d54677891138f585ea15acdc109a31d6942
FrostyBoys <bsd-12@BSD-12s-iMac.local>
2016-10-07 12:42:47 +0800

    added games and updated files

---
+++ Bomb/js/index.js
@@ -0,0 +1,11 @@
+function mango() {
+  alert("Hi, i'm the talking mango!");
+}
+
+function grass() {
+  alert("Hi, i'm the talking grass!");
+}
+
+function time_bomb() {
+  alert("Why are you trying to time this bomb?")
+}
